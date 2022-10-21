3553bc5928b3e72350dffa641d287c73629fc80f
levijmason <levijackmason@gmail.com>
2022-10-20 19:41:49 -0500

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
-import { CssBaseline } from "@nextui-org/react";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
-      styles: [<>{initialProps.styles}</>],
+      styles: <>{initialProps.styles}</>
     };
   }
 
