2339f3fac3395a5a4108f727a830b22b3b06fc23
levijmason <levijackmason@gmail.com>
2024-01-18 23:30:04 -0600

    Refactor projects page to fetch data from CMS

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,52 +1,18 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { getProjects } from "@/app/lib/cms/actions/get-projects";
+import { ProjectCard } from "@/app/ui/features/project-card";
 
-export default function Projects() {
+export default async function Projects() {
+  const data = await getProjects();
+
   return (
-    <section className="justify-evenly w-full">
-      <h2 className="text-4xl font-bold">Previous projects</h2>
+    <div className="flex flex-col w-full space-y-14">
+      <h2 className="text-4xl font-bold pt-8">Projects</h2>
       <br />
-      <p className="text-xl">
-        Here are a few of the projects I&apos;ve worked on over the past few
-        years.
-      </p>
-      <br />
-      <Card className="p-4 mb-4">
-        <CardHeader className="pb-0 pt-2 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Daily Mix</p>
-          <small className="text-default-500">12 Tracks</small>
-          <h4 className="font-bold text-large">Frontend Radio</h4>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2"></CardBody>
-      </Card>
-      <Card className="p-4 mb-4">
-        <CardHeader className="pb-0 pt-2 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Daily Mix</p>
-          <small className="text-default-500">12 Tracks</small>
-          <h4 className="font-bold text-large">Frontend Radio</h4>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2"></CardBody>
-      </Card>
-      <Card className="p-4 mb-4">
-        <CardHeader className="pb-0 pt-2 flex-col items-start">
-          <p className="text-tiny uppercase font-bold">Daily Mix</p>
-          <small className="text-default-500">12 Tracks</small>
-          <h4 className="font-bold text-large">Frontend Radio</h4>
-        </CardHeader>
-        <CardBody className="overflow-visible py-2"></CardBody>
-      </Card>
-
-      {/* <p>
-        I&apos;m a seasoned professional with over five years of experience in
-        music business and tech consulting. With a strong foundation in
-        full-stack development and experience at Endava, a global IT
-        consultancy, I have built websites and applications from start to
-        finish, delivering exceptional user experiences. As the founder of Trip
-        the Fan, a thriving music management firm, I excels in developing
-        strategic partnerships, curating exceptional artist experiences, and
-        overseeing effective promotional campaigns. My ability to seamlessly
-        blend music business and tech consulting makes me a valuable asset,
-        bringing a unique perspective and wealth of experience to drive success.
-      </p> */}
-    </section>
+      <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+        {data.map((p: any) => {
+          return <ProjectCard key={p.id} data={p} />;
+        })}
+      </div>
+    </div>
   );
 }
