58dfb03e78bb789d5c01112af18b6f6772953df0
levijmason <levijackmason@gmail.com>
2024-01-18 23:42:52 -0600

    Update ProjectCard component props in Projects page

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -10,7 +10,7 @@ export default async function Projects() {
       <br />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {data.map((p: any) => {
-          return <ProjectCard key={p.id} data={p} />;
+          return <ProjectCard key={p.id} data={p} featured={false} />;
         })}
       </div>
     </div>
