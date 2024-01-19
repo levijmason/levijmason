ee7d161380efa2a2dc9868b015ca3c1b827a4056
levijmason <levijackmason@gmail.com>
2024-01-18 23:31:44 -0600

    Update loading component styles

--- app/(pages)/loading.tsx
+++ app/(pages)/loading.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@nextui-org/spinner";
 export default function Loading() {
   // Or a custom loading skeleton component
   return (
-    <div className="flex flex-col min-h-screen content-center">
+    <div className="flex flex-col min-h-screen content-center items-center justify-center justify-items-center">
       <Spinner label="Loading..." size="lg" />
     </div>
   );
