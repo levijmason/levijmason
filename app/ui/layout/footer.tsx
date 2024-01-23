a4895f524f208b9bb942204bfae05ffcf5671407
levijmason <levijackmason@gmail.com>
2024-01-23 02:52:58 -0600

    Remove unused code and update SocialLinks component in Footer

--- app/ui/layout/footer.tsx
+++ app/ui/layout/footer.tsx
@@ -1,14 +1,11 @@
-import { getLinks } from "@/app/lib/cms/actions";
 import { SocialLinks } from "@/ui/layout";
 import { Divider } from "@nextui-org/divider";
 
 export const Footer = async () => {
-  const links = await getLinks();
-
   return (
     <footer className="flex flex-col items-center p-8 md:px-28 w-full h-32 bg-background">
       <Divider className="my-14 rounded-full" />
-      <SocialLinks links={links} />
+      <SocialLinks />
       <p className="text-foreground pt-12 pb-16">
         <b>©</b> {new Date().getFullYear()}, <b>Levi J. Mason</b>
       </p>
