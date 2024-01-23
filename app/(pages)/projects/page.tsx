75fa5c223563c06345a1cfacdb656c17ef57ece9
levijmason <levijackmason@gmail.com>
2024-01-23 03:01:09 -0600

    Update projects page heading

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -17,7 +17,7 @@ export default async function Projects() {
 
   return (
     <div className="flex flex-col w-full space-y-14">
-      <h2 className="text-4xl font-bold pt-8">Projects</h2>
+      <h2 className="text-4xl font-bold pt-8">Apps & Websites</h2>
       <br />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {data.map((p: Project) => {
