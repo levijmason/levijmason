1772656fb5ed0e0e6526abb0ac6ba2f3d460e238
levijmason <levijackmason@gmail.com>
2024-01-23 16:37:21 -0600

    Rename Projects to Tech

--- app/(pages)/tech/page.tsx
+++ app/(pages)/tech/page.tsx
@@ -1,7 +1,7 @@
-import { Project, getProjects } from "@/app/lib/cms/actions/get-projects";
-import { ProjectCard } from "@/app/ui/features/project-card";
+import { Project, getProjects } from "@/lib/cms/actions";
+import { ProjectCard } from "@/ui/features/";
 
-export default async function Projects() {
+export default async function Tech() {
   const data = await getProjects();
 
   // Sort data by featured (true / false / null) last, and sort them alphabetically
@@ -16,14 +16,19 @@ export default async function Projects() {
   );
 
   return (
-    <div className="flex flex-col w-full space-y-14">
-      <h2 className="text-4xl font-bold pt-8">Apps & Websites</h2>
+    <section className="flex flex-col w-full space-y-14 md:px-16">
+      <div className="flex flex-col gap-8 max-w-prose md:py-12">
+        <h2 className="text-4xl font-bold pt-8">Apps & Websites 💻</h2>
+        <p className="text-lg md:text-xl font-light leading-loose tracking-wide">
+          These are some things on the internet that I&apos;ve developed.
+        </p>
+      </div>
       <br />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
         {data.map((p: Project) => {
           return <ProjectCard key={p.name} data={p} featured={false} />;
         })}
       </div>
-    </div>
+    </section>
   );
 }
