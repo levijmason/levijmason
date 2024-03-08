13ffcbf6f6379e1e1eb9d4822205af045845fb95
Levi Mason <levijackmason@gmail.com>
2024-03-08 17:52:32 -0600

    Add "./ui/**/*.{js,ts,jsx,tsx,mdx}" to content array in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./ui/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
